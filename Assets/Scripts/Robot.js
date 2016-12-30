97f1a9b52cc9953fdd3aa0d39d171ccbdaf539d9
John Pizzo <jjpizzo4@gmail.com>
2016-12-29 21:43:49 -0500

    .gitignore

---
+++ Assets/Scripts/Robot.js
@@ -0,0 +1,45 @@
+﻿#pragma strict
+
+private var animator: Animator;
+public var speed: float = 0.5;
+public var rotationSpeed: float = 0.5;
+
+function Start () {
+	animator = this.GetComponent(Animator);
+}
+
+function Update () {
+	if(Input.GetKey(KeyCode.W))
+		moveForward();
+	else if(Input.GetKey(KeyCode.S))
+		moveBackward();
+	else
+		animator.SetInteger('Speed', 0);
+
+	if(Input.GetKey(KeyCode.A))
+		turnLeft();
+	else if(Input.GetKey(KeyCode.D))
+		turnRight();
+	
+}
+
+private function turnLeft() {
+	const 
+	this.transform.rotation = ;
+}
+
+private function turnRight() {
+	this.transform.rotation.y += this.rotationSpeed;
+}
+
+private function moveForward() {
+	animator.SetInteger('Speed', 2);
+	this.transform.position.z -= this.speed;
+	this.transform.rotation.y = 180;
+}
+
+private function moveBackward() {
+	animator.SetInteger('Speed', 2);
+	this.transform.position.z += this.speed;
+	this.transform.rotation.y = 0;
+}

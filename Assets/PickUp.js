3ca0ecda052ca3350b58003d191be88327f06a1f
John Pizzo <jjpizzo4@gmail.com>
2016-12-30 21:54:32 -0500

    merge

---
+++ Assets/PickUp.js
@@ -0,0 +1,24 @@
+﻿#pragma strict
+
+private var attached: boolean = false;
+
+function Start () {
+	
+}
+
+function Update () {
+	if(this.attached) {
+		this.transform.localPosition = new Vector3(0.038, 0.0127, -0.0066);
+		this.transform.localRotation.eulerAngles = new Vector3(-20.286, 90, -90); 
+		this.GetComponent(Rigidbody).useGravity = false;
+	}
+}
+
+public function OnCollisionEnter(col: Collision) {
+	
+	if(col.gameObject.tag == 'Player') {
+//	Debug.Log();
+		this.transform.parent = col.gameObject.transform.Find('Armature/Hips/Spine/Chest/Arm_Upper_R/Arm_Lower_R/Hand_R');
+		this.attached = true;
+	}
+}
